5189405c630ecc8d4c2798a33672c42f16444df3
swap54 <krswapnil84@gmail.com>
2022-07-15 16:19:15 +0530

    first commit

---
+++ src/components/login.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState } from "react";
+import Axios from 'axios';
+function Login(){
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+    const [user,setUser] = useState("");
+    const handleLogin = (e) =>{
+        Axios({
+            method:"post",
+            data:{
+                username:username,
+                password:password,
+            },
+            withCredentials:true,
+            url: "http://localhost:3001/login"
+        }).then((res)=>{
+            if(res.data.message){
+                alert(res.data.message)
+            }
+            else{
+                setUser(res.data.username);
+                
+            }
+        });
+        e.preventDefault();
+    }
+    return(
+        <>  
+            {
+                user ? <h1>Welcome {user}</h1>:null
+            }
+            <form>
+                <label>Login</label>
+                <input type="text" placeholder="Email" onChange={(e)=>setUsername(e.target.value)}/>
+                <input type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
+                <button onClick={handleLogin}>Login</button>
+            </form>
+        </>
+    );
+}
+
+export default Login;
---
+++ src/components/register.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useState } from "react";
+import Axios from 'axios';
+import {useNavigate} from 'react-router-dom';
+import './style.css';
+function Register(){
+    const navigate = useNavigate();
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+    const handleRegister = (e) =>{
+        // Axios.post("http://localhost:3001/register",{username:username,password:password}).then((response)=>{
+        //     alert(response.data);
+        // })
+        Axios({
+            method:"POST",
+            data:{
+                username:username,
+                password:password,
+            },
+            withCredentials:true,
+            url: "http://localhost:3001/register"
+        }).then((res)=>{
+            if(res.data.message){
+                alert(res.data.message);
+            }
+            else{
+                alert(res.data);
+                navigate('/login');
+            }
+        });
+        e.preventDefault();
+    }
+    return(
+        <>
+            <form>
+                <label>Register</label>
+                <input type="text" placeholder="Email" onChange={(e)=>setUsername(e.target.value)}/>
+                <input type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
+                <button onClick={handleRegister}>Register</button>
+            </form>
+        </>
+    );
+}
+
+export default Register;
